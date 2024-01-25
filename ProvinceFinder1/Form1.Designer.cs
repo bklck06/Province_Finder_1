199f212bcd45f0fac580de1e3f626ceaace8f644
Buğra Kağan <89995984+bklck06@users.noreply.github.com>
2024-01-25 14:49:42 +0300

    Province_Finder_1
    Recognizing Turkish provinces using license plate codes.

---
+++ ProvinceFinder1/Form1.Designer.cs
@@ -0,0 +1,144 @@
+﻿namespace ProvinceFinder1
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button1Bul = new System.Windows.Forms.Button();
+            this.button2Delete = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(25, 50);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(71, 16);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Şehir İsmi: ";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(25, 111);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(81, 16);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Plaka kodu: ";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(28, 69);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(144, 22);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // textBox2
+            // 
+            this.textBox2.BackColor = System.Drawing.SystemColors.HighlightText;
+            this.textBox2.Location = new System.Drawing.Point(28, 139);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(144, 22);
+            this.textBox2.TabIndex = 1;
+            // 
+            // button1Bul
+            // 
+            this.button1Bul.Location = new System.Drawing.Point(28, 212);
+            this.button1Bul.Name = "button1Bul";
+            this.button1Bul.Size = new System.Drawing.Size(116, 23);
+            this.button1Bul.TabIndex = 2;
+            this.button1Bul.Text = "Bul";
+            this.button1Bul.UseVisualStyleBackColor = true;
+            this.button1Bul.Click += new System.EventHandler(this.button1Bul_Click);
+            // 
+            // button2Delete
+            // 
+            this.button2Delete.BackColor = System.Drawing.Color.IndianRed;
+            this.button2Delete.Location = new System.Drawing.Point(161, 212);
+            this.button2Delete.Name = "button2Delete";
+            this.button2Delete.Size = new System.Drawing.Size(116, 23);
+            this.button2Delete.TabIndex = 2;
+            this.button2Delete.Text = "Sil";
+            this.button2Delete.UseVisualStyleBackColor = false;
+            this.button2Delete.Click += new System.EventHandler(this.button2Delete_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label3.Location = new System.Drawing.Point(43, 9);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(234, 20);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "- Türkiye Province Finder -";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(289, 257);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.button2Delete);
+            this.Controls.Add(this.button1Bul);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Turkiye_Province_Finder1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button1Bul;
+        private System.Windows.Forms.Button button2Delete;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ ProvinceFinder1/Form1.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace ProvinceFinder1
+{
+    public partial class Form1 : Form
+    {
+        private Dictionary<int, string> plateCodeToProvince;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            plateCodeToProvince = new Dictionary<int, string>
+        {
+            {01, "Adana"}, {02, "Adıyaman"}, {03, "Afyonkarahisar"}, {04, "Ağrı"}, {05, "Amasya"},
+            {06, "Los-Angaras"}, {07, "Antalya"}, {08, "Artvin"}, {09, "Aydın"}, {10, "Balıkesir"},
+            {11, "Bilecik"}, {12, "Bingöl"}, {13, "Bitlis"}, {14, "Bolu"}, {15, "Burdur"},
+            {16, "Bursa"}, {17, "Çanakkale"}, {18, "Çankırı"}, {19, "Çorum"}, {20, "Denizli"},
+            {21, "Diyarbakır"}, {22, "Edirne"}, {23, "Elazığ"}, {24, "Erzincan"}, {25, "Erzurum"},
+            {26, "Eskişehir"}, {27, "Gaziantep"}, {28, "Giresun"}, {29, "Gümüşhane"}, {30, "Hakkari"},
+            {31, "Hatay"}, {32, "Isparta"}, {33, "İçel (Mersin)"}, {34, "İstanbul"}, {35, "İzmir"},
+            {36, "Kars"}, {37, "Kastamonu"}, {38, "Kayseri"}, {39, "Kırklareli"}, {40, "Kırşehir"},
+            {41, "Kocaeli"}, {42, "Konya"}, {43, "Kütahya"}, {44, "Malatya"}, {45, "Manisa"},
+            {46, "Kahramanmaraş"}, {47, "Mardin"}, {48, "Muğla"}, {49, "Muş"}, {50, "Nevşehir"},
+            {51, "Niğde"}, {52, "Ordu"}, {53, "Rize"}, {54, "Sakarya"}, {55, "Samsun"},
+            {56, "Siirt"}, {57, "Sinop"}, {58, "Sivas"}, {59, "Tekirdağ"}, {60, "Tokat"},
+            {61, "Trabzon"}, {62, "Tunceli"}, {63, "Şanlıurfa"}, {64, "Uşak"}, {65, "Van"},
+            {66, "Yozgat"}, {67, "Zonguldak"}, {68, "Aksaray"}, {69, "Bayburt"}, {70, "Karaman"},
+            {71, "Kırıkkale"}, {72, "Batman"}, {73, "Şırnak"}, {74, "Bartın"}, {75, "Ardahan"},
+            {76, "Iğdır"}, {77, "Yalova"}, {78, "Karabük"}, {79, "Kilis"}, {80, "Osmaniye"},
+            {81, "Düzce"}
+        };
+        }
+
+        private void button1Bul_Click(object sender, EventArgs e)
+        {
+            if (plateCodeToProvince != null) 
+            {
+                if (int.TryParse(textBox2.Text, out int plateCode))
+                {
+                   
+                    if (plateCodeToProvince.TryGetValue(plateCode, out string province))
+                    {
+                        
+                        textBox1.Text = province;
+                    }
+                    else
+                    {
+                        MessageBox.Show("Geçersiz plaka kodu. Lütfen 1-81 arasında bir değer girin.", "Hata", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("Plaka kodu bir sayı olmalıdır.", "Hata", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+            else
+            {
+                MessageBox.Show("Sözlük yüklenirken bir hata oluştu.", "Hata", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2Delete_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = "";
+            textBox2.Text = "";
+        }
+    }
+}
